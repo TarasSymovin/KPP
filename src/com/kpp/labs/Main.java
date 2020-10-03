4bf004bf8ae296804e03d3ed440e95c7d210e8c0
TARAS <taras.simovin97@gmail.com>
2020-10-03 15:09:43 +0300

    commit

--- src/com/kpp/labs/Main.java
+++ src/com/kpp/labs/Main.java
@@ -3,6 +3,7 @@ package com.kpp.labs;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Shoes shoes = new SummerShoes("Кроси adidas", 42, 1300);
+        shoes.printParam();
     }
 }
